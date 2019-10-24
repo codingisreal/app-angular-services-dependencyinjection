0e544272311ad34451c319f3fde1b790bd7886d9
Gaurav Wairagade <55729932+codingisreal@users.noreply.github.com>
2019-10-24 14:03:26 +0530

    Updated Changes to show elapsed time

--- src/app/Services/app.serverhub.service.ts
+++ src/app/Services/app.serverhub.service.ts
@@ -1,12 +1,21 @@
 import { ServerD } from "../Model/app.server.model";
-import { Injectable } from "@angular/core";
+import { Injectable, OnInit, OnDestroy } from "@angular/core";
 
-export class ServerHub {
+export class ServerHub implements OnInit {
   servers: ServerD[] = [];
   server: ServerD;
   noServerNotify: string = "No servers to Show";
+  timer = setInterval(() => {
+    this.clearDead();
+  }, 1000);
   constructor() {
     //console.log(this.servers.length)
+  }
+
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    this.timer = null;
   }
 
   addServer(serverName: string, validity: number) {
@@ -14,5 +23,15 @@ export class ServerHub {
     this.server.name = serverName;
     this.server.validity = validity;
     this.servers.push(this.server);
+  }
+
+  clearDead() {
+    this.servers.forEach(function(value, index) {
+      console.log(index);
+      if (value.validity > 0) {
+        value.validity = value.validity - 1;
+      }
+      //this.servers = this.servers.splice(index, 1);
+    });
   }
 }
